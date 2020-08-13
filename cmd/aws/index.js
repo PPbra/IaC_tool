364b354a4208d9622015d33c4025d728a2405b93
ppbra <kaka020998@gmail.com>
2020-08-13 11:18:56 +0700

    add gen instance code

--- cmd/aws/index.js
+++ cmd/aws/index.js
@@ -1,13 +1,30 @@
 const readline = require("readline-sync");
+const { ec2 } = require("../../config/aws/config");
 const aws_ec2 = require("../../config/aws/config").ec2;
 
+aws_ec2.config.map((e)=>{
+        const value = readline.question(`What ${e.type}?(Enter for default!):\n`);
+        if(!!value&&value!==`\n`){
+            e.value = value;
+        }else{
+            e.value = e.default;
+        }   
+})
+const ec2_config = {};
+aws_ec2.config.map((e)=>{
+    ec2_config[e.type] = e.value;
+})
+
+console.log(ec2_config);
 const terraform_code = `provider "aws" {
     region = "ap-southest-2"
-}` 
+}
+${aws_ec2.generator(ec2_config)}
+` 
 
-aws_ec2.map((e)=>{
-        
-})
+console.log(terraform_code);
+
+
 
 // const ami_name =  readline.question("ami_name:");
 
