7cdb6502e3935f9584a1d33272412a02c94adb31
ppbra <kaka020998@gmail.com>
2020-08-19 14:13:50 +0700

    edit config type

--- cmd/aws/index.js
+++ cmd/aws/index.js
@@ -1,34 +1,57 @@
+#!/usr/bin/env node
 const readline = require("readline-sync");
-const { ec2 } = require("../../config/aws/config");
-const aws_ec2 = require("../../config/aws/config").ec2;
+const commander = require("commander");
 
-aws_ec2.config.map((e)=>{
-        const value = readline.question(`What ${e.type}?(Enter for default!):\n`);
-        if(!!value&&value!==`\n`){
-            e.value = value;
-        }else{
-            e.value = e.default;
-        }   
-})
-const ec2_config = {};
-aws_ec2.config.map((e)=>{
-    ec2_config[e.type] = e.value;
-})
+// const { ec2 } = require("../../config/aws/config");
+// const aws_ec2 = require("../../config/aws/config").ec2;
 
-console.log(ec2_config);
-const terraform_code = `provider "aws" {
-    region = "ap-southest-2"
+const fs = require("fs");
+
+
+
+commander
+    .option("-f,--file <path to file>","Use JSON config file to generate terraform code!");
+
+
+
+
+
+commander.parse(process.argv)
+
+
+try {
+    const config_path = !!commander.file?commander.file:"";
+    const ec2_config = JSON.parse(fs.readFileSync(config_path));
+    console.log(ec2_config)
+} catch (error) {
+    console.log(error)
 }
-${aws_ec2.generator(ec2_config)}
-` 
 
-console.log(terraform_code);
+
+// aws_ec2.config.map((e)=>{
+//         const value = readline.question(`What ${e.type}?(Enter for default!):\n`);
+//         if(!!value&&value!==`\n`){
+//             e.value = value;
+//         }else{
+//             e.value = e.default;
+//         }   
+// })
+// const ec2_config = {};
+// aws_ec2.config.map((e)=>{
+//     ec2_config[e.type] = e.value;
+// })
+
+// const terraform_code = `provider "aws" {
+//     region = "ap-southest-2"
+// }
+// ${aws_ec2.generator(ec2_config)}
+// ` 
+
+// console.log(terraform_code);
 
 
 
 // const ami_name =  readline.question("ami_name:");
-
-
 // console.log(ami_name);
 // console.log(aws_ec2);
 
--- routes/test-gen/test.js
+++ routes/test-gen/test.js
@@ -1,6 +1,24 @@
 const express = require("express");
-const test_route = express.Router();
+const readline = require("readline");
+const test_router = express.Router();
 
+test_router.get('/test',(req,res) =>{
+    
+})
 
+const rl  = readline.createInterface({
+    input: process.stdin,
+    output:process.stdout
+})
 
-module.exports = test_route;
+var hello = "a"
+const dosomething = ()=>{
+    console.log("hahaha!");
+    rl.question("aws_ami name?",(hello_input)=>{
+        hello = hello_input
+    })
+    console.log(hello);
+}
+
+dosomething()
+module.exports = test_router;
