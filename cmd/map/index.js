c467379a9baa7f6b1b25e99a5f3a2e917cc8993c
ppbra <kaka020998@gmail.com>
2020-10-09 17:47:16 +0700

    mapping instance

--- cmd/map/index.js
+++ cmd/map/index.js
@@ -1,9 +1,13 @@
 const aws_gen = require("../../config/aws");
+const gcp_gen = require("../../config/gcp")
 console.log(aws_gen.aws_instance.mapping({
             type:"instance",
             name:"main_instance",
             instance_type:"micro",
             os:"ubuntu",
-            network:"default_network",
+            network_interface:{
+                vpc:"default_network",
+                subnet:"subnet_1"
+            },
             disk:"disk_1"
 }))
